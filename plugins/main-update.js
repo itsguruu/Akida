72d138e7a74ace4f8e493a589db6ffc4d8e5b6f0
GURU <lylasofia462@gmail.com>
2025-10-11 04:51:33 +0300

    GURU

--- plugins/main-update.js
+++ plugins/main-update.js
@@ -81,7 +81,7 @@ malvin({
   if (!isOwner) return reply("❌ Owner only command!");
   
   try {
-    const repoUrl = config.REPO || "https://github.com/betingrich4/Mercedes";
+    const repoUrl = config.REPO || "https://github.com/itsguruu/Akida";
     const repoApiUrl = repoUrl.replace('github.com', 'api.github.com/repos');
     
     // Get current local version
